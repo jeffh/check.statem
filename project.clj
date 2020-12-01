aa62d42a287d806511b7d648aec4d4d7a8c0d2d6
Jeff Hui <jeff@jeffhui.net>
2020-11-30 22:12:19 -0800

    Update test.check version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/test.check "0.10.0"]
+                 [org.clojure/test.check "1.1.0"]
                  [org.clojure/math.combinatorics "0.1.6"]]
   :plugins [[lein-codox "0.10.7"]]
   :codox {:metadata   {:doc/format :markdown}
